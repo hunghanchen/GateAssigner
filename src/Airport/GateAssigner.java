f2f2ca169ed9d72e811f126c125c6b82ad858339
hunghanchen <chenhung@sheridancollege.ca>
2019-04-16 13:26:54 -0400

    FlightAssign

--- src/Airport/GateAssigner.java
+++ src/Airport/GateAssigner.java
@@ -78,8 +78,6 @@ public final class GateAssigner extends Stage {
         StringBuilder builderFlightInfo = new StringBuilder();
         CreateRandomAccessFileAndAssignTime();
 
-//        String[] xs = new String[]{"a", "b", "c", "d"};
-//        return xs;
         try {
             Scanner fileFlight = new Scanner(file);
             while (fileFlight.hasNextLine()) {
@@ -120,6 +118,7 @@ public final class GateAssigner extends Stage {
 
     private void CreateRandomAccessFileAndAssignTime() {
         // create the april 30 days array (we only use the april)
+        String[] arrFlightInfoPart = new String[9];
         String[] gate = {"Gate1", "Gate2"};
         Integer[] intArrayDate = new Integer[30];
         for (int i = 0; i < intArrayDate.length; i++) {
@@ -146,16 +145,103 @@ public final class GateAssigner extends Stage {
                     }
                 }
             }
-            raf.seek(RECORD_SIZE);
+            raf.seek(0);
             StringBuilder builder = new StringBuilder();
             for (int i = 0; i < GATE_SIZE; i++) {
                 builder.append(raf.readChar());
             }
             System.out.println(builder);
             //------------------------------------------(above is create the standard)
-
+            //----------------------------------(below start to read every date write into the raf)
             File file = new File("Flight Information.txt");
-            
+
+            try {
+                Scanner fileFlight = new Scanner(file);
+                int i = 0;
+                while (fileFlight.hasNextLine()) {
+                    arrFlightInfoPart = fileFlight.nextLine().split(",");
+                    String[] strArrFlightAssignArrivalDate = new String[]{arrFlightInfoPart[3].trim()};//2019-04-08
+                    strArrFlightAssignArrivalDate[i] = strArrFlightAssignArrivalDate[i].substring(8);//catch 08
+                    int[] intArrConvertStrDepDateToInt = new int[]{Integer.valueOf(strArrFlightAssignArrivalDate[i])};//String 08 convert to integer 6
+                    String[] strArrFlightAssignArrivalTime = new String[]{arrFlightInfoPart[4].trim()};//01 : 02
+                    int[] intArrFlightAssignArrivalTime = new int[]{Integer.valueOf(strArrFlightAssignArrivalTime[i].substring(0, 2))};//String 01 to integer to 1
+
+                    String[] strArrFlightAssignDepartDate = new String[]{arrFlightInfoPart[5].trim()};//2019-04-08
+                    strArrFlightAssignDepartDate[i] = strArrFlightAssignDepartDate[i].substring(8);
+                    int[] intArrConvertStrArrDateToInt = new int[]{Integer.valueOf(strArrFlightAssignDepartDate[i])};
+                    String[] strArrFlightAssignDepartTime = new String[]{arrFlightInfoPart[6].trim()};//01 : 02
+                    int[] intArrFlightAssignDepartTime = new int[]{Integer.valueOf(strArrFlightAssignDepartTime[i].substring(0, 2))};
+
+                    //here to convert fligt no. string into integer
+                    String[] strArrFlightNo = new String[]{arrFlightInfoPart[2].trim()};//String flight no. "6384" into integer 6384
+                    int[] intArrConvertStrNumberToInt = new int[]{Integer.valueOf(strArrFlightNo[i])};
+                    System.out.println(intArrConvertStrNumberToInt[0]);
+
+//                    StringBuilder buildertest = new StringBuilder();
+//                    for (int x = 0; x < GATE_SIZE; x++) {
+//                        buildertest.append(raf.readChar());
+//                    }
+//                    System.out.println(buildertest);
+//                    buildertest.setLength(0);
+//                    System.out.println(intArrConvertStrDepDateToInt[0]);
+//                    System.out.println(intArrFlightAssignArrivalTime[0]);
+//
+                    //write flight number to the clock if not zero will sent to the gate2 to raf
+                    raf.seek(0);
+                    raf.skipBytes((GATE_SIZE * 2)
+                            + ((DATE_SIZE * 2 + CLOCK_SIZE * 24) * (intArrConvertStrDepDateToInt[i] - 1))
+                            + (DATE_SIZE * 2) + (intArrFlightAssignArrivalTime[i] * CLOCK_SIZE));
+                    if (raf.readInt() == 0) {
+                        raf.seek(0);
+                        raf.skipBytes((GATE_SIZE * 2)
+                                + ((DATE_SIZE * 2 + CLOCK_SIZE * 24) * (intArrConvertStrDepDateToInt[i] - 1))
+                                + (DATE_SIZE * 2) + (intArrFlightAssignArrivalTime[i] * CLOCK_SIZE));
+                        raf.writeInt(intArrConvertStrNumberToInt[i]);
+                    } else {
+                        raf.seek(RECORD_SIZE);
+                        raf.skipBytes((GATE_SIZE * 2)
+                                + ((DATE_SIZE * 2 + CLOCK_SIZE * 24) * (intArrConvertStrDepDateToInt[i] - 1))
+                                + (DATE_SIZE * 2) + (intArrFlightAssignArrivalTime[i] * CLOCK_SIZE));
+                        raf.writeInt(intArrConvertStrNumberToInt[i]);
+                    }
+
+                    //write arrival date to raf
+                    raf.seek(0);
+
+                    raf.skipBytes((GATE_SIZE * 2)
+                            + ((DATE_SIZE * 2 + CLOCK_SIZE * 24) * (intArrConvertStrDepDateToInt[i] - 1))
+                            + (DATE_SIZE * 2) + (intArrFlightAssignArrivalTime[i] * CLOCK_SIZE));
+                    System.out.println("This is raf" + raf.readInt());
+
+//                    i++;
+//                    System.out.println(intArrConvertStrArrDateToInt[0]);
+//                String flight = part[0] + "," + part[1] + "," + part[2];
+                    //we need to put in the combox only need for the three character of the flight type
+//                    for (int j = 0; j < 3; j++) {
+//                        String flightType = part[0].trim();
+//                        charFlight = flightType.charAt(j);
+//                        builderFlightInfo.append(charFlight);
+//                    }
+//                    for (int j = 0; j < 3; j++) {
+//                        if (j == 0) {
+//                            String flightType = part[1].trim();
+//                            charFlight = Character.toUpperCase(flightType.charAt(j));
+//                            builderFlightInfo.append(charFlight);
+//                        } else {
+//                            String flightType = part[1].trim();
+//                            charFlight = flightType.charAt(j);
+//                            builderFlightInfo.append(charFlight);
+//                        }
+//                    }
+//                    builderFlightInfo.append(part[2].trim());
+//                    outputPart[i] = builderFlightInfo.toString();
+//                    i++;
+//                    builderFlightInfo.setLength(0);
+                }
+
+            } catch (FileNotFoundException ex) {
+                System.out.println(ex.toString());
+            }
 
         } catch (IOException ex) {
             System.out.println(ex.toString());
